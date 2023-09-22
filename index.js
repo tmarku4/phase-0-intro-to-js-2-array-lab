50cabce5851369b6c724b8e94c9dc5d078be57aa
tommarku <tmarku4@gmail.com>
2023-09-22 11:18:11 -0400

    Adds arraylab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(catName) {
+    cats.push(catName);
+  }
+  function destructivelyPrependCat(catName) {
+    cats.unshift(catName);
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.splice(0, 1);
+  }
+  function appendCat(catName) {
+    cats.push(catName);
+  }
+  constcats = ["Milo", "Otis", "Garfield"];
+
+  function appendCat(name) {
+    return [...cats, name];
+  }
+  constcats = ["Milo", "Otis", "Garfield"];
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+function removeLastCat() {
+    const newCats = [...cats];
+      newCats.pop();
+      return newCats;
+  }
+  function removeFirstCat() {
+    const newCats = [...cats];
+      newCats.shift();
+      return newCats;
+  }
